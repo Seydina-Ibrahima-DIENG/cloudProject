00b7c4c534fb70e1718b6a29e6b40f8e47942e51
Seydina-Ibrahima-DIENG <seydinaibrahima.dieng@uadb.edu.sn>
2026-03-30 19:17:07 +0100

    Add MySQL database connection and user route

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const PORT = 3000;
 
+// Connexion à la base de données (VM2)
+const db = mysql.createPool({
+    host: '192.168.100.20', // IP de la VM base de données
+    user: 'nodeuser',
+    password: 'password',
+    database: 'cloud_project'
+});
+
 // Servir le fichier HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route pour récupérer les utilisateurs depuis MySQL
+app.get('/users', async (req, res) => {
+    try {
+        const [rows] = await db.query("SELECT * FROM users");
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erreur base de données");
+    }
 });
 
 // Lancer le serveur
